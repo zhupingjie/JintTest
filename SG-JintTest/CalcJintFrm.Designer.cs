81a7ba53157693737b38882d5bc482e4cf29a576
zhupingjie <36753031+zhupingjie@users.noreply.github.com>
2018-02-23 13:57:43 +0800

    commit

---
+++ SG-JintTest/CalcJintFrm.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace SG_JintTest
+{
+    partial class CalcJintFrm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ParamCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ParamValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.BlueViolet;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 32);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(3, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "请输入公式内容：";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.dataGridView1);
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 363);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(800, 140);
+            this.panel3.TabIndex = 9;
+            // 
+            // dataGridView1
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("微软雅黑", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ParamCode,
+            this.ParamValue});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 20;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(686, 140);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // ParamCode
+            // 
+            this.ParamCode.HeaderText = "参数编码";
+            this.ParamCode.Name = "ParamCode";
+            this.ParamCode.Width = 180;
+            // 
+            // ParamValue
+            // 
+            this.ParamValue.HeaderText = "参数值";
+            this.ParamValue.Name = "ParamValue";
+            this.ParamValue.Width = 320;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.btnCalc);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel4.Location = new System.Drawing.Point(686, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(114, 140);
+            this.panel4.TabIndex = 1;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnCalc.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnCalc.Location = new System.Drawing.Point(0, 0);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(114, 140);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "计算公式";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBox2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBox2.Location = new System.Drawing.Point(0, 302);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(800, 61);
+            this.richTextBox2.TabIndex = 10;
+            this.richTextBox2.Text = "";
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.BlueViolet;
+            this.panel5.Controls.Add(this.label2);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel5.Location = new System.Drawing.Point(0, 270);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(800, 32);
+            this.panel5.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(3, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 26);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "计算后公式结果：";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("微软雅黑", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 32);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(800, 238);
+            this.richTextBox1.TabIndex = 13;
+            this.richTextBox1.Text = "";
+            // 
+            // CalcJintFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 503);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Name = "CalcJintFrm";
+            this.Text = "LiftNext Jint v1.1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ParamCode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ParamValue;
+    }
+}
+
---
+++ SG-JintTest/CalcJintFrm.cs
@@ -0,0 +1,166 @@
+﻿using Jint;
+using Jint.Parser;
+using Jint.Runtime;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SG_JintTest
+{
+    public partial class CalcJintFrm : Form
+    {
+        public CalcJintFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_Enter(object sender, EventArgs e)
+        {
+            this.richTextBox1.SelectAll();
+        }
+
+        string GetFunc()
+        {
+            return @"function sin(num)
+{
+    return Math.sin(num);
+}
+function cos(num)
+{
+    return Math.cos(num);
+}
+function tan(num) {
+    return Math.tan(num);
+}
+function Asin(num) {
+    return Math.asin(num);
+}
+function Acos(num) {
+    return Math.acos(num);
+}
+function Atan(num) {
+    return Math.atan(num);
+}
+function ROUNDDOWN(num1, num2) {
+    return Math.floor(num1 * Math.pow(10, num2)) / Math.pow(10, num2);
+}
+function ROUNDUP(num1, num2) {
+    return Math.ceil(num1 * Math.pow(10, num2)) / Math.pow(10, num2);
+}
+
+function CEILING(num1, num2) {
+    if (num2 == 0) return num2;
+
+    var m = num1 / num2;
+    var n = Math.ceil(m);
+    return num2 * n;
+}
+function FLOOR(num1, num2)
+{
+    if (num2 == 0) return num2;
+
+    var m = num1 / num2;
+    var n = Math.floor(m);
+    return num2 * n;
+}
+function cont(num)
+{
+    for (var i= 1; i < arguments.length; i++)
+    {
+        if (num == arguments[i])
+            return true;
+    }
+    return false;
+}
+function Pi() {
+    return Math.PI;
+}
+function Abs(num) {
+    return Math.abs(num);
+}
+function INT(num) {
+    return parseInt(num);
+}
+function log(msg) {
+    throw 'Error:' + msg;
+}
+function GetDate() {
+    var date = new Date();
+    var y = date.getFullYear();
+    var m = date.getMonth() + 1;
+    var d = date.getDate();
+
+    return y + '' + (m.length == 1 ? '0' + m : m) + '' + (d.length == 1 ? '0' + d : d);
+}";
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            Engine engine = new Engine();
+            var exp = this.richTextBox1.Text.Trim();
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                DataGridViewRow row = dataGridView1.Rows[i];
+                DataGridViewCell cell1 = row.Cells[0];
+                DataGridViewCell cell2 = row.Cells[1];
+
+                if (cell1.Value == null || cell2.Value == null) continue;
+
+                var code = cell1.Value.ToString();
+                var value = cell2.Value.ToString();
+
+                if (!string.IsNullOrEmpty(code) && !string.IsNullOrEmpty(value))
+                {
+                    try
+                    {
+                        engine.SetValue(code, value);
+                    }
+                    catch (Exception ex)
+                    {
+                        this.richTextBox2.Text = string.Format("设置参数[{0}]的值[{1}]错误", code, value);
+                        return;
+                    }
+                }
+            }
+            try
+            {
+                engine.Execute(GetFunc() + " " + exp);
+                var result = engine.GetCompletionValue();
+                if (result == null || result.ToString() == "undefined")
+                {
+                    this.richTextBox2.Text = "值为空";
+                }
+                else
+                {
+                    this.richTextBox2.Text = result.ToString();
+                }
+            }
+            catch (ParserException px)
+            {
+                this.richTextBox2.Text = "语法错误:" + px.Message;
+            }
+            catch (JavaScriptException jx)
+            {
+                if (jx.Message.Contains("is not defined"))
+                {
+                    var pname = jx.Message.Replace(" is not defined", "");
+                    this.richTextBox2.Text = string.Format("参数[{0}]尚未赋值", pname);
+                }
+                else
+                {
+                    this.richTextBox2.Text = "语法错误";
+                }
+            }
+            catch(Exception ex)
+            {
+                this.richTextBox2.Text = "未知错误:" + ex.Message;
+            }
+        }
+    }
+}
